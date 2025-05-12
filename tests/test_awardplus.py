9c6c560d4f1c8bc406f0df57d28f5123cee25242
narumi <toucans-cutouts0f@icloud.com>
2025-05-12 22:36:49 +0800

    update test case

--- tests/test_awardplus.py
+++ tests/test_awardplus.py
@@ -7,8 +7,8 @@ from tripplus.awardplus import RedemptionRequest
 
 @pytest.mark.parametrize("ori", ["TPE"])
 @pytest.mark.parametrize("dst", ["KIX"])
-@pytest.mark.parametrize("cabin", ["y", "c"])
-@pytest.mark.parametrize("type", ["ow", "rt"])
+@pytest.mark.parametrize("cabin", ["y"])
+@pytest.mark.parametrize("type", ["rt"])
 def test_redemption_request(ori: str, dst: str, cabin: Literal["y", "c", "f"], type: Literal["ow", "rt"]) -> None:
     req = RedemptionRequest(ori=ori, dst=dst, cabin=cabin, type=type)
     resp = req.do()
