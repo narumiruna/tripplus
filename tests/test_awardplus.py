c081b8f07c37003143a351c16433626af3c73fcd
narumi <toucans-cutouts0f@icloud.com>
2025-05-12 22:19:32 +0800

    add test_redemption_request

---
+++ tests/test_awardplus.py
@@ -0,0 +1,18 @@
+import pytest
+
+from tripplus.awardplus import RedemptionRequest
+
+
+@pytest.mark.parametrize("ori", ["TPE"])
+@pytest.mark.parametrize("dst", ["KIX"])
+@pytest.mark.parametrize("cabin", ["y", "c"])
+@pytest.mark.parametrize("type", ["ow", "rt"])
+def test_redemption_request(ori: str, dst: str, cabin: str, type: str) -> None:
+    req = RedemptionRequest(ori=ori, dst=dst, cabin=cabin, type=type)
+    resp = req.do()
+    assert len(resp.items) > 0
+    for item in resp.items:
+        assert item.origin == ori
+        assert item.destination == dst
+        assert item.cabin == cabin
+        assert item.type == type
